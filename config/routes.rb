bc35864d0da5388bfb792bf05dfa6b9792548357
Gopal Othayoth <cerebraldatabank@tamu.edu>
2024-02-07 20:13:59 +0000

    Create Activities controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'activities/index'
+  get 'activities/show'
+  get 'activities/new'
+  get 'activities/edit'
+  get 'activities/delete'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
