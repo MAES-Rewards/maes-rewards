552c8db40dcaecb316174d428c95c07d12dfa5ec
James Beeber <jhbeeber@gmail.com>
2024-02-08 05:58:52 +0000

    Added oauth client id/secret

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  ENV['GOOGLE_OAUTH_CLIENT_ID'] = '364768825078-vitdos9mokebf1fl3k9cja5onlrkmob8.apps.googleusercontent.com'
+  ENV['GOOGLE_OAUTH_CLIENT_SECRET'] = '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'
 end
