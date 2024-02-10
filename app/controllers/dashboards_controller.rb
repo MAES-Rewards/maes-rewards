9498d5af5aa0269b806289586b34589421ac8a15
jaejin <jaejin0109@gmail.com>
2024-02-10 20:28:38 +0000

    added show page

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,7 +2,18 @@ class DashboardsController < ApplicationController
   before_action :authenticate_admin!
 
   def member
-    # Additional logic here if needed
+    @user = User.where(:is_admin => false).first
+    # admin = Admin.from_google(**from_google_params)
+  
+    # if admin.present?
+    #   sign_out_all_scopes
+    #   flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
+      
+    #   @user = User.find_by(email: admin.email)
+    # else
+    #   flash[:alert] = t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
+    #   redirect_to new_admin_session_path
+    # end
   end
 
   def admin
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @users = User.order(:name)
+    
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
