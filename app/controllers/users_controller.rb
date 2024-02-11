3f44f0159e37f223c458b6daa1e18bbd66060126
ShahirAliGit <shahirali@tamu.edu>
2024-02-11 15:41:39 -0600

    finished being able to update/edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,18 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = 'User was successfully updated.'
+      if session[:is_admin]
+        redirect_to(admin_dashboard_path)
+      else
+        redirect_to(user_path(@user))
+      end
+    else
+      flash[:alert] = 'User could not be updated. Attribute(s) are invalid.'
+      render('edit', status: :unprocessable_entity)
+    end
   end
 
   def delete
@@ -23,5 +35,14 @@ class UsersController < ApplicationController
   def destroy
     User.find(params[:id]).destroy
     redirect_to destroy_admin_session_path
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :email,
+      :points,
+      :is_admin
+    )
   end
 end
