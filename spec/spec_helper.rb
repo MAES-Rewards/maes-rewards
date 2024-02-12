e9df829b9e207d603b46736b0dc36960e4644e8d
Anna Hartman <ahartman03@tamu.edu>
2024-02-12 15:14:38 -0600

    Back to chrome again

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,12 +21,13 @@ require 'rack/test'
 
 SimpleCov.start
 
-Capybara.register_driver :rack_test do |app|
-  Capybara::RackTest::Driver.new(app, respect_data_method: true, redirect_limit: 20)
+Capybara.register_driver :selenium_chrome_headless do |app|
+  options = Selenium::WebDriver::Chrome::Options.new(args: %w[--headless --disable-gpu --no-sandbox])
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options).tap do |driver|
+    driver.browser.logger.level = :debug
+  end
 end
 
-Capybara.default_driver = :rack_test
-Capybara.javascript_driver = :rack_test
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
