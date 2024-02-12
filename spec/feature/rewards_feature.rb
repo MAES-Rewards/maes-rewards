97e9efb9517d13604c532f343bb9c86a50167fb7
Anna Hartman <ahartman03@tamu.edu>
2024-02-11 20:44:30 -0600

    View and create reward integration tests

--- spec/feature/rewards_feature.rb
+++ spec/feature/rewards_feature.rb
@@ -16,14 +16,54 @@ RSpec.describe('Viewing rewards', type: :feature) do
     Capybara.default_max_wait_time = 10 # Adjust the wait time as needed
   end
 
-  it 'user logs in with Google' do
+  it 'user logs in with Google & views rewards' do
     visit new_admin_session_path
     click_on 'Sign in via Google'
     click_on 'View Rewards'
 
     # Assuming there is some delay or asynchronous operation happening
     # If content doesn't appear immediately, wait for it
-    expect(page).to(have_content('Welcome, John Doe!'))
+    expect(page).to(have_content('All of the rewards that can be purchased with points are shown below.'))
+  end
+end
+
+RSpec.describe('Creating rewards', type: :feature) do
+  before do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+      provider: 'google_oauth2',
+      uid: '123456',
+      info: {
+        email: 'ahartman03@tamu.edu',
+        name: 'Sally Doe'
+      }
+    }
+                                                                      )
+    Capybara.current_driver = :selenium
+    Capybara.default_max_wait_time = 10 # Adjust the wait time as needed
+  end
+
+  it 'user logs in with Google & creates reward' do
+    visit new_admin_session_path
+    click_on 'Sign in via Google'
+    click_on 'View Rewards'
+    visit new_reward_path
+
+    fill_in 'reward[name]', with: 'Test Reward'
+    fill_in 'reward[point_value]', with: 10
+    fill_in 'reward[dollar_price]', with: 20.99
+    fill_in 'reward[inventory]', with: 50
+    click_on 'Create Reward'
+
+    visit rewards_path
+
+    expect(page).to(have_content('Test Reward'))
+    expect(page).to(have_content('10'))
+    expect(page).to(have_content('20.99'))
+    expect(page).to(have_content('50'))
+
+    # Assuming there is some delay or asynchronous operation happening
+    # If content doesn't appear immediately, wait for it
   end
 end
 
