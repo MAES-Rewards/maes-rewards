49b2a7f6e1071c1432f10e0468fcd4433468ad96
Anna Hartman <ahartman03@tamu.edu>
2024-02-11 21:13:22 -0600

    Added admin log in test, delete test

--- spec/feature/login_feature.rb
+++ spec/feature/login_feature.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe('Google OAuth login', type: :feature) do
+RSpec.describe('Google OAuth MEMBER login', type: :feature) do
   before do
     OmniAuth.config.test_mode = true
     OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
@@ -16,12 +16,40 @@ RSpec.describe('Google OAuth login', type: :feature) do
     Capybara.default_max_wait_time = 10 # Adjust the wait time as needed
   end
 
-  it 'user logs in with Google' do
+  it 'user logs in with Google to member page' do
     visit new_admin_session_path
     click_on 'Sign in via Google'
 
     # Assuming there is some delay or asynchronous operation happening
     # If content doesn't appear immediately, wait for it
     expect(page).to(have_content('Welcome, John Doe!'))
+  end
+end
+
+RSpec.describe('Google OAuth ADMIN login', type: :feature) do
+  before do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+      provider: 'google_oauth2',
+      uid: '114332087672869564965',
+      info: {
+        email: 'ahartman03@tamu.edu',
+        name: 'Anna Hartman'
+      }
+    }
+                                                                      )
+    Capybara.current_driver = :selenium
+    Capybara.default_max_wait_time = 10 # Adjust the wait time as needed
+  end
+
+  it 'user logs in with Google into admin page' do
+    visit new_admin_session_path
+    click_on 'Sign in via Google'
+    visit admin_dashboard_path
+    # Assuming there is some delay or asynchronous operation happening
+    # If content doesn't appear immediately, wait for it
+    expect(page).to(have_content('ADMIN Page'))
+    expect(page).to(have_content('Activities'))
+    # Activities is only displayed for Admin.
   end
 end
--- spec/feature/rewards_feature.rb
+++ spec/feature/rewards_feature.rb
@@ -46,6 +46,7 @@ RSpec.describe('Creating rewards', type: :feature) do
   it 'user logs in with Google & creates reward' do
     visit new_admin_session_path
     click_on 'Sign in via Google'
+    visit admin_dashboard_path
     click_on 'View Rewards'
     visit new_reward_path
 
@@ -64,6 +65,46 @@ RSpec.describe('Creating rewards', type: :feature) do
 
     # Assuming there is some delay or asynchronous operation happening
     # If content doesn't appear immediately, wait for it
+  end
+end
+
+RSpec.describe('Deleting rewards', type: :feature) do
+  before do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+      provider: 'google_oauth2',
+      uid: '123456',
+      info: {
+        email: 'ahartman03@tamu.edu',
+        name: 'Sally Doe'
+      }
+    }
+                                                                      )
+    Capybara.current_driver = :selenium
+    Capybara.default_max_wait_time = 10 # Adjust the wait time as needed
+  end
+
+  it 'user logs in with Google & creates then deletes reward' do
+    visit new_admin_session_path
+    click_on 'Sign in via Google'
+    visit admin_dashboard_path
+    click_on 'View Rewards'
+    visit new_reward_path
+
+    fill_in 'reward[name]', with: 'Test Reward'
+    fill_in 'reward[point_value]', with: 10
+    fill_in 'reward[dollar_price]', with: 20.99
+    fill_in 'reward[inventory]', with: 50
+    click_on 'Create Reward'
+
+    visit rewards_path
+
+    click_on 'Delete'
+
+    click_on 'Delete Reward'
+
+    expect(page).not_to(have_content('Test Reward'))
+    expect(page).to(have_content('Reward was successfully deleted'))
   end
 end
 
