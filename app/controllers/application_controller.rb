1d6e6811ab7c83c5f8ebefba73df030845504c91
Anna Hartman <ahartman03@tamu.edu>
2024-02-26 16:35:10 +0000

    Admin view now has menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+
 class ApplicationController < ActionController::Base
   before_action :authenticate_admin!
+  helper_method :render_menu
+
+  def render_menu
+    if session[:is_admin]
+      render partial:'layouts/adminmenu'
+    elsif session[:is_admin]
+      render partial:'layouts/membermenu'
+    end
+  end
 end
