d52ba3edbce3b2d57db6ec1196b8a57a1bdac9ff
jaejin <jaejin0109@gmail.com>
2024-02-27 06:10:23 -0600

    implemented spend_transaction show controller

--- app/controllers/spend_transactions_controller.rb
+++ app/controllers/spend_transactions_controller.rb
@@ -1,4 +1,17 @@
 class SpendTransactionsController < ApplicationController
   def show
+    @spend_transactions = SpendTransaction.where(user_id: params[:id]).order(:created_at)
+    @user = User.find(params[:id])
+    @rewards = []
+
+    if @spend_transactions.length > 0
+      n = @spend_transactions.length()
+      i = 0
+      loop do
+        @rewards.append(Reward.find(@spend_transactions[i].reward_id))
+        i += 1
+        break if i >= n
+      end
+    end
   end
 end
