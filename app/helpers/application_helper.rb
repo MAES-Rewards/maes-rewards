ba8ad57df543fc142d0b52dc059b792fb6b60ce9
ShahirAliGit <shahirali@tamu.edu>
2024-03-29 18:55:53 -0500

    added support for mobile checking

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+    def device
+        agent = request.user_agent
+        return "mobile" if agent =~ /Mobile/
+        return "desktop"
+    end
 end
