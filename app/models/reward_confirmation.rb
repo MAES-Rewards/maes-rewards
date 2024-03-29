50e445a17088298df3664867538120e717f6b191
James Beeber <jhbeeber@gmail.com>
2024-03-29 02:58:19 +0000

    Created relationships between reward confirmations and spend transactions

---
+++ app/models/reward_confirmation.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class RewardConfirmation < ApplicationRecord
+    belongs_to :spend_transaction
+  end
+  
--- app/models/spend_transaction.rb
+++ app/models/spend_transaction.rb
@@ -3,6 +3,7 @@
 class SpendTransaction < ApplicationRecord
   belongs_to :user
   belongs_to :reward
+  has_one :reward_confirmation, dependent: :destroy
   # has_one :user, dependent: :destroy
   # has_one :reward, dependent: :destroy
 end
