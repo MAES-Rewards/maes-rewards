8c8b21731538e1a6fcfedfbc450ecf5d46406ed5
James Beeber <jhbeeber@gmail.com>
2024-04-01 08:37:33 +0000

    Added comments to controllers

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,18 +1,22 @@
 # frozen_string_literal: true
 
 class DashboardsController < ApplicationController
+  # Ensure that only officers can access officer dashboard and member is authorized based on session variable
   before_action :admin_check, only: [:admin]
   before_action :authorize_user, only: [:member]
 
+  # Show the member dashboard based on user ID number
   def member
     # @user = User.where(:is_admin => false).where(email: params[:email])
     @user = User.find(params[:user_id])
   end
 
+  # Show the admin dashboard and provides a list of all members
   def admin
     @users = User.where(is_admin: false)
   end
 
+  # Check admin session variable to determine which dashboard to send the user
   def show
     if session[:is_admin]
       redirect_to(admin_dashboard_path)
--- app/controllers/reward_confirmations_controller.rb
+++ app/controllers/reward_confirmations_controller.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
 
 class RewardConfirmationsController < ApplicationController
+  # Ensure that only officers can access certain notification functionalities
   before_action :admin_check, only: %I[index update]
   before_action :set_reward_confirmation, only: [:update]
 
+  # List reward purchases in the past 24 hours
   def index
     @recent_purchases = SpendTransaction.includes(:reward, :user, :reward_confirmation)
                                         .where('created_at >= ?', 24.hours.ago).order(created_at: :desc)
   end
 
+  # Update status of reward purchase based on if the officer clicks the checkbox or not
   def update
     if @reward_confirmation.update(reward_confirmation_params)
       respond_to do |format|
@@ -21,11 +24,13 @@ class RewardConfirmationsController < ApplicationController
 
   private
 
+  # Redirect back to reward notifications page if purchase isn't found
   def set_reward_confirmation
     @reward_confirmation = RewardConfirmation.find_by(id: params[:id])
     redirect_to(reward_confirmations_path, alert: 'Error.') if @reward_confirmation.nil?
   end
 
+  # Require specific parameters for reward confirmations (boolean reward_given value)
   def reward_confirmation_params
     params.require(:reward_confirmation).permit(:reward_given)
   end
