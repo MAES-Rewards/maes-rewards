6cb97f3aa786eba18e0e4f8f3eba5004bce7ea00
Gopal Othayoth <cerebraldatabank@tamu.edu>
2024-04-21 14:54:56 +0000

    Hotfix: fix Rails models to allow txns with deleted users/rewards/activities

--- app/models/spend_transaction.rb
+++ app/models/spend_transaction.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class SpendTransaction < ApplicationRecord
-  belongs_to :user
-  belongs_to :reward
+  belongs_to :user, optional: true
+  belongs_to :reward, optional: true
   has_one :reward_confirmation, dependent: :destroy
   # has_one :user, dependent: :destroy
   # has_one :reward, dependent: :destroy
